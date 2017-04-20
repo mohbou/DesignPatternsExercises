3139ff9098e634cf3051700ce4e831e038aa185d
mohbou <a.bourkaib@gmail.com>
2017-04-20 11:11:09 -0400

    first

---
+++ commadPatterns/example1/Main.java
@@ -0,0 +1,8 @@
+package main.commadPatterns.example1;
+
+public class Main {
+    public static void main(String[] args) {
+        Light bedRoomLight = new Light();//receiver
+        AirConditioner airConditioner = new AirConditioner();
+    }
+}
