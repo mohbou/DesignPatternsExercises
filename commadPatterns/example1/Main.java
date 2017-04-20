a6c2d49b64d750e23df6b4c823db30a6c097c9ef
mohbou <a.bourkaib@gmail.com>
2017-04-20 11:28:47 -0400

    first

--- commadPatterns/example1/Main.java
+++ commadPatterns/example1/Main.java
@@ -1,8 +1,31 @@
 package main.commadPatterns.example1;
 
+
 public class Main {
     public static void main(String[] args) {
+
+        //stuff in house =  Receiver
         Light bedRoomLight = new Light();//receiver
-        AirConditioner airConditioner = new AirConditioner();
+        AirConditioner airConditioner = new AirConditioner();//receiver
+
+        // on commands - concrete commands
+        LightOn turnBedRoomLightOn = new LightOn(bedRoomLight);
+        AirConditionerOn turnAirConditionerOn = new AirConditionerOn(airConditioner);
+
+        // off commands - concrete commands
+        LightOff turnBedRoomLightOff = new LightOff(bedRoomLight);
+        AirConditionerOff turnAirConditionerOff= new AirConditionerOff(airConditioner);
+
+        //Invoker
+        RemoteController remote = new RemoteController();
+        remote.insertNewOnCommand(turnBedRoomLightOn);
+        remote.insertNewOnCommand(turnAirConditionerOn);
+        remote.insertNewOffCommand(turnBedRoomLightOff);
+        remote.insertNewOffCommand(turnAirConditionerOff);
+
+        remote.pressButtonOn(0);
+        remote.pressButtonOn(1);
+        remote.pressButtonOff(0);
+        remote.pressButtonOff(1);
     }
 }
