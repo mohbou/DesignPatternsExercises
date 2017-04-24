5087c153c5478efdf35c691634476e549dd87cbc
mohbou <a.bourkaib@gmail.com>
2017-04-24 08:38:32 -0400

    finish Bridge

---
+++ bridgepattern/example2/DrawAPI.java
@@ -0,0 +1,5 @@
+package main.bridgepattern.example2;
+
+public interface DrawAPI {
+    public void drawCircle(int radius, int x, int y);
+}
---
+++ bridgepattern/example2/RedCircle.java
@@ -0,0 +1,8 @@
+package main.bridgepattern.example2;
+
+public class RedCircle implements DrawAPI {
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+        System.out.println("Drawing Circle[ color: red, radius: " + radius + ", x: " + x + ", " + y + "]");
+    }
+}
