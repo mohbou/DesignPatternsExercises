a5d57947e14bf6dcb4649b7a1d766807da6e0aac
mohbou <a.bourkaib@gmail.com>
2017-04-26 12:01:10 -0400

    updating observer

--- observerPattern/example1/WeatherStationSubject.java
+++ observerPattern/example1/WeatherStationSubject.java
@@ -1,5 +1,6 @@
 package main.observerPattern.example1;
 
+//observable
 public interface WeatherStationSubject {
     public void addObserver(Observer observer);
     public void removeObserver(Observer observer);
