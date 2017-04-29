b5068c2ed0e8f7f029022e6957fc21d335cd982b
mohbou <a.bourkaib@gmail.com>
2017-04-29 13:46:26 -0400

    example 1 composite pattern

--- compositepattern/example1/EmployeeComponent.java
+++ compositepattern/example1/EmployeeComponent.java
@@ -2,14 +2,14 @@ package main.compositepattern.example1;
 
 public abstract class EmployeeComponent {
 
-    public String getName() {
-        return name;
-    }
-
     private String name;
 
     public EmployeeComponent(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public  void printSupervisor(int spacing) {
